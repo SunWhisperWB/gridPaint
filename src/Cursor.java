746ce65f37deaa5b93541cae348de41635572822
Hugo Monteiro <hugoze@netcabo.pt>
2019-06-14 11:13:24 +0100

    added initial setup git log git status

---
+++ src/Cursor.java
@@ -0,0 +1,21 @@
+
+public class Cursor {
+
+    private Position pos;
+
+    public Cursor(){
+        pos = new Position(0, 0);
+    }
+
+
+    public void paint(){
+
+    }
+
+    public void erase(){
+
+    }
+
+
+
+}
